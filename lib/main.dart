5f4f26b9e6bdfba041bee99a62a94fbe6c1c6533
Lix <lixiao.dev@gmail.com>
2020-01-08 17:57:28 +0800

    feat: update dart grammar

--- lib/main.dart
+++ lib/main.dart
@@ -35,59 +35,43 @@ class MyApp extends StatelessWidget {
       )
     );
     
-    Column buildButtonColumn(IconData icon, String label) {
-      Color color = Theme.of(context).primaryColor;
 
-      return new Column(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          new Icon(icon, color: color),
-          new Container(
-            margin: const EdgeInsets.only(top: 8.0),
-            child: new Text(
-              label,
-              style: new TextStyle(
-                fontSize: 12.0,
-                fontWeight: FontWeight.w400,
-                color: color
-              )
-            )
-          )
-        ]
-      );
-    }
+    Color color = Theme.of(context).primaryColor;
 
-    Widget buttonSection = new Container(
-      child: new Row(
+    Widget buttonSection = Container(
+      child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          buildButtonColumn(Icons.call, 'CALL'),
-          buildButtonColumn(Icons.near_me, 'ROUTE'),
-          buildButtonColumn(Icons.share, 'SHARE')
-        ],
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE')
+        ]
       )
     );
 
-    Widget textSection = new Container(
+    Widget textSection = Container(
       padding: const EdgeInsets.all(32.0),
-      child: new Text(
-        '''
-Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
-        ''',
+      child: Text(
+        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Alps. Situated 1,578 meters above sea level, it is one of the '
+        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+        'half-hour walk through pastures and pine forest, leads you to the '
+        'lake, which warms to 20 degrees Celsius in the summer. Activities '
+        'enjoyed here include rowing, and riding the summer toboggan run.',
         softWrap: true,
       )
     );
 
-    return new MaterialApp(
+    return MaterialApp(
       title: 'Flutter layout demo',
       home: Scaffold(
         appBar: AppBar(title: Text('Flutter layout demo')),
-        body: new ListView(
+        body: ListView(
           children: [
-            new Image.asset(
+            Image.asset(
               'images/lake.jpg',
-              height: 240.0,
+              width: 600,
+              height: 240,
               fit: BoxFit.cover
             ),
             titleSection,
@@ -95,8 +79,28 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
             textSection
           ]
         )
-      ),
-      theme: new ThemeData(primarySwatch: Colors.blue),
+      )
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w400,
+              color: color
+            )
+          )
+        )
+      ]
     );
   }
 }
