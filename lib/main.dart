29f5f48ac1936b0bcc19ea2151e8ddc5f5513177
Lix <lixiao.dev@gmail.com>
2020-01-09 14:42:19 +0800

    feat: create not metarial app

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,15 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        appBar: AppBar(title: Text('Flutter layout demo')),
-        body: Center(
-          child: Text('Hello World')
-        )
-      )
+    return Container(
+      decoration: BoxDecoration(color: Colors.white),
+      child: Center(
+        child: Text(
+          'Hello World',
+          textDirection: TextDirection.ltr,
+          style: TextStyle(fontSize: 32, color: Colors.black87)
+        ),
+      ),
     );
   }
 }
