6f246aa3fff58f0c1eb02a9237890621ec1d79c6
Lix <lixiao.dev@gmail.com>
2020-01-09 14:25:13 +0800

    :sparkles: will using layout code

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,10 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/Lix/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/Lix/Documents/WorkDemo/helloflutter"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/Lix/Documents/WorkDemo/helloflutter/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_FRAMEWORK_DIR=/Users/Lix/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
