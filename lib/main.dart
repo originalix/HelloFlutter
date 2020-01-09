3fc0b0599c040461354871955143fa90a3f0a08a
Lix <lixiao.dev@gmail.com>
2020-01-09 15:34:46 +0800

    feat: row and column layout

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,37 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(color: Colors.white),
-      child: Center(
-        child: Text(
-          'Hello World',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(fontSize: 32, color: Colors.black87)
+    return MaterialApp(
+      title: 'Flutter layout demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter layout demo'),
         ),
+        // Change to buildColumn() for the other column example
+        body: Center(child: buildColumn()),
       ),
+    );
+  }
+
+  Widget buildRow() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('images/1.jpg', fit: BoxFit.cover, width: 100, height: 100),
+        Image.asset('images/1.jpg', fit: BoxFit.cover, width: 100, height: 100),
+        Image.asset('images/1.jpg', fit: BoxFit.cover, width: 100, height: 100),
+      ],
+    );
+  }
+
+  Widget buildColumn() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Image.asset('images/1.jpg', fit: BoxFit.cover, width: 100, height: 100),
+        Image.asset('images/1.jpg', fit: BoxFit.cover, width: 100, height: 100),
+        Image.asset('images/1.jpg', fit: BoxFit.cover, width: 100, height: 100),
+      ],
     );
   }
 }
