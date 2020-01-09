9d25766b7683720ac837a98ac5ae1d44b312546c
Lix <lixiao.dev@gmail.com>
2020-01-09 16:31:08 +0800

    feat: icon list

--- lib/main.dart
+++ lib/main.dart
@@ -52,15 +52,60 @@ class MyApp extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
         stars,
-        Text('170 Reviews',
-            style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w800,
-                fontFamily: 'Roboto',
-                letterSpacing: 0.5,
-                fontSize: 20))
+        Text(
+          '170 Reviews',
+          style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.w800,
+              fontFamily: 'Roboto',
+              letterSpacing: 0.5,
+              fontSize: 20
+          )
+        )
       ],
     ),
+  );
+
+  static final descTextStyle = TextStyle(
+    color: Colors.black,
+    fontWeight: FontWeight.w800,
+    fontFamily: 'Roboto',
+    letterSpacing: 0.5,
+    fontSize: 18,
+    height: 2
+  );
+
+  final iconList = DefaultTextStyle.merge(
+    style: descTextStyle,
+    child: Container(
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              Icon(Icons.kitchen, color: Colors.green[500]),
+              Text('PREP'),
+              Text('25 min')
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.timer, color: Colors.green[500]),
+              Text('COOK'),
+              Text('1 hr')
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.restaurant, color: Colors.green[500]),
+              Text('FEEDS'),
+              Text('4-6')
+            ],
+          ),
+        ],
+      )
+    )
   );
 
   Widget buildColumn() {
